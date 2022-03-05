9b2583d3e7a52c28a01a3b02c30bd9420c410f59
Julian Schill <j.schill@web.de>
2022-03-05 16:59:10 +0000

    Implemented new API for ledcontrol 2.0

--- ledcontrol.py
+++ ledcontrol.py
@@ -5,8 +5,7 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
 import logging
-import os
-import asyncio
+import json
 from tornado.httpclient import AsyncHTTPClient
 
 class LEDControl:
@@ -18,58 +17,76 @@ class LEDControl:
         self.addr = config.get('address')
 
     async def set_led(self, 
+                preset = '',
+                group = 'main',
                 brightness='',
-                color_temp='',
                 saturation='',
-                primary_pattern='', 
-                primary_speed='', 
-                primary_scale='',
-                secondary_pattern='',
-                secondary_speed='',
-                secondary_scale='',
-                palette=''
+                pattern='', 
+                speed='', 
+                scale='',
+                palette='',
+                global_brightness='',
+                global_saturation=''
                 ):
-
-        calls=[]
-        if (brightness):
-            calls.append(self.set_param('brightness',brightness))
-        if (color_temp):
-            calls.append(self.set_param('color_temp',color_temp))
-        if (saturation):
-            calls.append(self.set_param('saturation',saturation))
-        if (primary_pattern):
-            calls.append(self.set_param('primary_pattern',primary_pattern))
-        if (primary_speed):
-            calls.append(self.set_param('primary_speed',primary_speed))
-        if (primary_scale):
-            calls.append(self.set_param('primary_scale',primary_scale))
-        if (secondary_pattern):
-            calls.append(self.set_param('secondary_pattern',secondary_pattern))
-        if (secondary_speed):
-            calls.append(self.set_param('secondary_speed',secondary_speed))
-        if (secondary_scale):
-            calls.append(self.set_param('secondary_scale',secondary_scale))
-        if (palette):
-            calls.append(self.set_param('palette',palette))
         
-        await asyncio.gather(*calls)
-        return "done"
-
-    async def set_param(self, key, value):
-        url = f"{self.addr}/setparam?key={key}&value={value}"
-        data=""        
         http_client = AsyncHTTPClient()
+        payload={}
+        payload["groups"]={}
+        if preset: 
+            url = f"{self.addr}/getpresets"
+            
+            try:
+                response = await http_client.fetch(url, method='GET', headers={"Content-Type": "application/json"})
+                presets=json.loads(response.body)
+                payload["groups"] = presets[preset]
+            except Exception:
+                msg = f"Ledcontrol: Error reading presets."
+                logging.exception(msg)
+                raise self.server.error(msg)
+        else:
+            url = f"{self.addr}/getsettings"
+            try:
+                response = await http_client.fetch(url, method='GET', headers={"Content-Type": "application/json"})
+                response_body=json.loads(response.body)
+                groups = response_body["groups"]
+                for g in groups:
+                    if groups[g]["name"] == group:
+                        group = g
+
+            except Exception:
+                msg = f"Ledcontrol: Error reading settings."
+                logging.exception(msg)
+                raise self.server.error(msg)
+
+            payload["groups"][group]={}
+            if (brightness):
+                payload["groups"][group]["brightness"] = float(brightness)
+            if (saturation):
+                payload["groups"][group]["saturation"] = float(saturation)
+            if (pattern):
+                payload["groups"][group]["function"] = int(pattern)
+            if (speed):
+                payload["groups"][group]["speed"] = float(speed)
+            if (scale):
+                payload["groups"][group]["scale"] = float(scale)
+            if (palette):
+                payload["groups"][group]["palette"] = int(palette)
+
+        if (global_brightness):
+            payload["global_brightness"] = float(global_brightness)
+        if (global_saturation):
+            payload["global_saturation"] = float(global_saturation)
+
+        url = f"{self.addr}/updatesettings"
+        body = json.dumps(payload)
         try:
-            response = await http_client.fetch(url)
-            data = response.body
+            response = await http_client.fetch(url, method='POST', headers={"Content-Type": "application/json"}, body=body)
         except Exception:
-            msg = f"Ledcontrol: Error setting parameter {key} to value {value}"
+            msg = f"Ledcontrol: Error setting parameter. Payload: {payload}"
             logging.exception(msg)
             raise self.server.error(msg)
 
+        return "done"
+
 def load_component(config):
     return LEDControl(config)
-
-
-
-
